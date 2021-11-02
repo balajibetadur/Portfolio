f6e62e2619ffefc6573b0e4b62c7c658b8c0b4bc
Balaji Betadur <balajibetadur@gmail.com>
2021-11-02 11:48:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,382 @@
+let data = {
+    'menu' : [
+        {class : 'uil uil-estate icon', 'text' : 'Home'},
+        {class : 'uil uil-user icon', 'text' : 'About'},
+        {class : 'uil uil-file-alt icon', 'text' : 'Skills'},
+        {class : 'uil uil-briefcase-alt icon', 'text' : 'Work'},
+        {class : 'uil uil-browser icon', 'text' : 'Projects'},
+        {class : 'uil uil-trophy icon', 'text' : 'Awards'},
+        {class : 'uil uil-message icon', 'text' : 'Contact'}
+
+    ],
+    'socialMedia' : [
+        'uil uil-linkedin-alt social__icon',
+        'uil uil-github social__icon',
+        'uil uil-envelope social__icon'
+    ],
+
+    'whatIdo' : [
+        {
+            'title' : 'Data Science & AI',
+            'image' : 'DSAI.svg',
+            'description' : [
+            '⚡Developing highly scalable production ready models for various deeplearning and statistical use cases',
+            '⚡ Experience of working with Computer Vision and NLP projects',            
+            '⚡ Complex quantitative modelling for dynamic forecasting and time series analysis'
+            ]
+        },
+        {
+            'title' : 'Web development',
+            'image' : 'web.svg',
+            'description' : [
+                '⚡ Building resposive websites for deploying Machine Learning algorithms',
+                '⚡ Developing front end using react',
+                '⚡ Creating simple UI using javascript, firebase, mongoDB, NodeJS, flask, bootstrap.'
+            ]
+        },{
+            'title' : 'Data Science & AI',
+            'image' : 'DSAI.svg',
+            'description' : [
+            '⚡Developing highly scalable production ready models for various deeplearning and statistical use cases',
+            '⚡ Experience of working with Computer Vision and NLP projects',            
+            '⚡ Complex quantitative modelling for dynamic forecasting and time series analysis'
+            ]
+        },{
+            'title' : 'Data Science & AI',
+            'image' : 'DSAI.svg',
+            'description' : [
+            '⚡Developing highly scalable production ready models for various deeplearning and statistical use cases',
+            '⚡ Experience of working with Computer Vision and NLP projects',            
+            '⚡ Complex quantitative modelling for dynamic forecasting and time series analysis'
+            ]
+        },{
+            'title' : 'Data Science & AI',
+            'image' : 'DSAI.svg',
+            'description' : [
+            '⚡Developing highly scalable production ready models for various deeplearning and statistical use cases',
+            '⚡ Experience of working with Computer Vision and NLP projects',            
+            '⚡ Complex quantitative modelling for dynamic forecasting and time series analysis'
+            ]
+        }
+    ],
+    'skills' : [
+        {'skill' : 'Pyhton', 'Level' : 400 * 0.9},
+        {'skill' : 'Machine Learning', 'Level' : 400 * 0.8},
+        {'skill' : 'Deep Learning', 'Level' : 400 * 0.8},
+        {'skill' : 'Keras', 'Level' : 400 * 0.7},
+        {'skill' : 'Tableau', 'Level' : 400 * 0.7},
+        {'skill' : 'Neural Networks', 'Level' : 400 * 0.75},
+        {'skill' : 'SQL', 'Level' : 400 * 0.8},
+        {'skill' : 'Flask', 'Level' : 400 * 0.8},
+        {'skill' : 'Javascript', 'Level' : 400 * 0.9},
+        {'skill' : 'ReactJS', 'Level' : 400 * 0.8},
+        {'skill' : 'MongoDB', 'Level' : 400 * 0.8},
+        {'skill' : 'DBMS', 'Level' : 400 * 0.9},
+        {'skill' : 'Firebasae', 'Level' : 400 * 0.8},
+        {'skill' : 'AWS', 'Level' : 400 * 0.5},
+        {'skill' : 'Bootstrap', 'Level' : 400 * 0.9},
+        {'skill' : 'Git', 'Level' : 400 * 0.8},
+        {'skill' : 'Mentoring', 'Level' : 400 * 1},
+        {'skill' : 'Teaching', 'Level' : 400 * 1}
+    ]
+    ,
+    'work' : 
+        {'Research_Internships' : [
+            {
+                'image' : 'nlp.jpeg',
+                'name' : 'Deep Learning Research Intern',
+                'location' : '@ Indian Institute of Technology, Guwahati',
+                'duration' : 'Sep 20 - Present',
+                'description' : ['test'],
+                'outcome' : ['test']
+            },
+            {
+                'image' : 'robot.jpg',
+                'name' : 'Deep Learning Research Intern',
+                'location' : '@ Indian Institute of Technology, Bhubaneshwar', 
+                'duration' : 'Sep 20 - Present',
+                'description' : ['test'],
+                'outcome' : ['test']
+            },
+            {
+                'image' : 'chatBot.jpg', 
+                'name' : 'Machine Learning Research Intern', 
+                'location' : '@ GIT, belgaum', 
+                'duration' : 'Sep 20 - Present',
+                'description' : ['test'],
+                'outcome' : 'jhgdf</br></br>sdfh'
+            },
+            {
+                'image' : 'farmer.jpg', 
+                'name' : 'NLP Research Intern', 
+                'location' : '@ GIT, belgaum', 
+                'duration' : 'Sep 20 - Present',
+                'description' : ['test'],
+                'outcome' : 'jhgdf</br></br>sdfh'
+            }
+        ],       
+        
+        'Industrial_Internships' :  [
+            {'image' : 'stockMarket.jpg',
+             'name' : 'Stock Market Automation Developer',
+             'location' : '@ Bluebery Capital',
+             'duration' : 'Sep 20 - Present',
+             'description' : 'test',
+             'outcome' : 'test'
+            },
+            {'image' : 'computerVision.jpg',
+             'name' : 'Computer Vision Intern',
+              'location' : '@ Naaniz Pvt Ltd',
+               'duration' : 'Sep 20 - Present',
+               'description' : 'test',
+               'outcome' : 'test'
+            },
+            {'image' : 'jobs.jpg',
+             'name' : 'Machine Learning Intern',
+              'location' : '@ Conscript Pvt ltd',
+               'duration' : 'Sep 20 - Present',
+               'description' : 'test',
+               'outcome' : 'test'
+            },
+            {'image' : 'webd.png',
+             'name' : 'Web development Intern',
+              'location' : '@ Uniq Technologies',
+               'duration' : 'Sep 20 - Present',
+               'description' : 'test',
+               'outcome' : 'test'
+            }
+        ],
+        
+        'Volunteerships' : [
+            {
+                'image' : 'campusHero.jpg', 
+                'name' : 'Campus Hero', 
+                'location' : '@ Girlscript Technologies', 
+                'duration' : 'Sep 20 - Present',
+                'description' : ['test'],
+                'outcome' : 'jhgdf</br></br>sdfh'
+            },
+            {
+                'image' : 'leader.jpg', 
+                'name' : 'Club Coordinator', 
+                'location' : '@ GIT, Belgaum', 
+                'duration' : 'Sep 20 - Present',
+                'description' : ['test'],
+                'outcome' : 'jhgdf</br></br>sdfh'
+            },
+            {
+                'image' : 'internshala.jpg', 
+                'name' : 'Internshala Student Partner', 
+                'location' : '@ Internshala', 
+                'duration' : 'Sep 20 - Present',
+                'description' : ['test'],
+                'outcome' : 'jhgdf</br></br>sdfh'
+            },
+            {
+                
+                'image' : 'mentor.jpg', 
+                'name' : 'Uplift Project Mentor', 
+                'location' : '@ Girlscript Technologies', 
+                'duration' : 'Sep 20 - Present',
+                'description' : ['test'],
+                'outcome' : 'jhgdf</br></br>sdfh'
+            },
+            {
+                'image' : 'onlineTeaching.jpg', 
+                'name' : 'Machine Learning Tranee', 
+                'location' : '@ Girlscript Foundation', 
+                'duration' : 'Sep 20 - Present',
+                'description' : ['test'],
+                'outcome' : 'jhgdf</br></br>sdfh'
+            },
+            {
+                'image' : 'teaching.jpg', 
+                'name' : 'Uplift Project Trainee', 
+                'location' : '@ Girlscript Technologies', 
+                'duration' : 'Sep 20 - Present',
+                'description' : ['test'],
+                'outcome' : 'jhgdf</br></br>sdfh'
+            }
+        ]
+        },
+        'projects' : [
+            {
+                'title' : 'Auto AI Modelling',
+                'purpose' : 'just',
+                'description' : 'none',
+                'outcome' : 'none too',
+                'image' : 'autoML.jpg'
+            },
+            {
+                'title' : 'COVID-19 prediction',
+                'purpose' : 'just',
+                'description' : 'none',
+                'outcome' : 'none too',
+                'image' : 'covid.jpg'
+            },
+            {
+                'title' : 'Lookinat - Abrod study prep website',
+                'purpose' : 'just',
+                'description' : 'none',
+                'outcome' : 'none too',
+                'image' : 'ms.jpg'
+            },
+            {
+                'title' : 'Moving Window Object detection',
+                'purpose' : 'just',
+                'description' : 'none',
+                'outcome' : 'none too',
+                'image' : 'object.png'
+            },
+            {
+                'title' : 'Automation',
+                'purpose' : 'just',
+                'description' : 'none',
+                'outcome' : 'none too',
+                'image' : 'automate.jpg'
+            },
+            {
+                'title' : 'Spotty - Desktop Assisstant',
+                'purpose' : 'just',
+                'description' : 'none',
+                'outcome' : 'none too',
+                'image' : 'spotty.jpg'
+            },
+            {
+                'title' : 'Human Machine Interaction System',
+                'purpose' : 'just',
+                'description' : 'none',
+                'outcome' : 'none too',
+                'image' : 'hm.jpg'
+            },
+            {
+                'title' : 'Buggy - Debugging programming errors',
+                'purpose' : 'just',
+                'description' : 'none',
+                'outcome' : 'none too',
+                'image' : 'debug.jpg'
+            },
+            {
+                'title' : 'Auto EDA',
+                'purpose' : 'just',
+                'description' : 'none',
+                'outcome' : 'none too',
+                'image' : 'eda.jpg'
+            }
+        ]
+
+
+}
+
+data.socialMedia.forEach(icon => {
+    document.getElementById('social__media').innerHTML += "<a href=''' target='_blank'><i class='"+icon+"'></i></a>"
+});
+
+data.menu.forEach(icon => {
+    document.getElementById('menu').innerHTML += "<div class='icon__text'><i class='"+icon.class+"'></i>" + icon.text + "</div>" 
+});
+
+let menuIcon = document.getElementById('open__menu')
+
+if (menuIcon){
+
+    menuIcon.addEventListener('click', () => {
+        document.getElementById('menu').classList.add('menu__show');
+    })
+}
+
+let closeIcon = document.getElementById('close__menu')
+
+if (closeIcon){
+
+    closeIcon.addEventListener('click', () => {
+        document.getElementById('menu').classList.remove('menu__show');
+    })
+}
+
+resumeBtn = document.getElementById('resume');
+
+if (resumeBtn){
+    resumeBtn.addEventListener('mouseenter', () => {
+        document.getElementById('resume__icon').classList.add('anim');
+    })
+    resumeBtn.addEventListener('mouseleave', () => {
+        document.getElementById('resume__icon').classList.remove('anim');
+    })
+}
+
+whatIDo = document.getElementById('about')
+
+data.whatIdo.forEach(item => {
+    whatIDo.innerHTML += '<div class="DSAI" id="DSAI"><p class="what__i__do__title" id="what__i__do__title">'+item.title+'</p><img class = "DSAI__img" id ="DSAI__img" src="images/'+item.image+'" alt="" srcset=""></div>';
+
+    item.description.forEach(desc => {
+        whatIDo.innerHTML += '</br><p class ="description" >'+desc+'</p>'
+    })
+})
+
+skillBars = document.getElementById('skillBars');
+
+data.skills.forEach(skill => {
+    skillBars.innerHTML += '<div class = "bars"><div class="fullBar" id="fullBar"><div style = "width: '+skill.Level+'px" class="bar" id="bar"> <p class="skills__text" id="skills__text">'+skill.skill+'</p></div></div></div>'
+})
+
+let works = document.getElementById('r__internships')
+
+data.work.Research_Internships.forEach(work => {
+    
+        works.innerHTML += '<div class="card" id="card"><div class="card__name" id="card__name">'+work.location+'</div> <img class = "card__img"src="images/'+work.image+'" alt="" srcset=""> <div class="details" id="details"> <p class="location" id="location"> '+work.name+'</p><p class="duration" id="duration"> '+work.duration+' </p> </div></div><div class="modal" id="modal"><div class="modal__content" id="modal-content"> <i class="uil uil-times modal__close close__ind"></i><p class="modal__title" id="modal__title">'+work.name+'</p> </br><p class="outcome" id="outcome">Description</p><hr>'+work.description+' <p class="outcome" id="outcome">Outcome</p><hr>'+work.outcome+'</div> </div>'
+    })
+
+works = document.getElementById('i__internships')
+
+data.work.Industrial_Internships.forEach(work => {
+    
+        works.innerHTML += '<div class="card" id="card"><div class="card__name" id="card__name">'+work.location+'</div> <img class = "card__img"src="images/'+work.image+'" alt="" srcset=""> <div class="details" id="details"> <p class="location" id="location"> '+work.name+'</p><p class="duration" id="duration"> '+work.duration+' </p> </div></div><div class="modal" id="modal"><div class="modal__content" id="modal-content"> <i class="uil uil-times modal__close"></i><p class="modal__title" id="modal__title">'+work.name+'</p></br> <p class="outcome" id="outcome">Description</p><hr>'+work.description+' <p class="outcome" id="outcome">Outcome</p><hr>'+work.outcome+'</div> </div>'
+
+        
+    })
+
+works = document.getElementById('v__internships')
+
+data.work.Volunteerships.forEach(work => {
+    
+        works.innerHTML += '<div class="card" id="card"><div class="card__name" id="card__name">'+work.location+'</div> <img class = "card__img"src="images/'+work.image+'" alt="" srcset=""> <div class="details" id="details"> <p class="location" id="location"> '+work.name+'</p><p class="duration" id="duration"> '+work.duration+' </p> </div></div><div class="modal" id="modal"><div class="modal__content" id="modal-content"> <i class="uil uil-times modal__close close__ind"></i><p class="modal__title" id="modal__title">'+work.name+'</p> <hr>'+work.description+'</div> </div>'
+    })
+
+let modals = document.querySelectorAll('.modal')
+let cards = document.querySelectorAll('.card')
+let close = document.querySelectorAll('.modal__close')
+
+
+cards.forEach((card,i) => card.addEventListener('click', () => {
+    modals[i].classList.add('block');
+}))
+
+
+close.forEach((card,i) => card.addEventListener('click', () => {
+    modals.forEach(modal => {
+        modal.classList.remove('block')
+    }
+    )}))
+    
+    let projects = document.getElementById('project__cards')
+    
+    data.projects.forEach(project => {
+        projects.innerHTML += '<div style="background-image: url(images/'+project.image+');" class="project__card" id="project__card"><p class="project__name" id="project__name">'+project.title+'</p></div><div class="project__modal" id="modal"><div class="modal__content" id="modal-content"> <i class="uil uil-times project__modal__close"></i><p class="modal__title" id="modal__title">'+project.title+'</p></br> <p class="outcome" id="outcome">Description</p><hr>'+project.description+' <p class="outcome" id="outcome">Purpose</p><hr>'+work.purpose+'<hr><p class="outcome" id="outcome">Outcome</p><hr>'+project.outcome+'</div> </div>'
+    })
+    
+let projectCards = document.querySelectorAll('.project__card');
+let projectModals = document.querySelectorAll('.project__modal')
+let projectClose = document.querySelectorAll('.project__modal__close')
+    
+    projectCards.forEach((card,i) => card.addEventListener('click', () => {
+        console.log('hu', projectCards)
+        projectModals[i].classList.add('block');
+}))
+
+projectClose.forEach((card,i) => card.addEventListener('click', () => {
+    projectModals.forEach(modal => {
+        modal.classList.remove('block')
+    }
+    )}))
