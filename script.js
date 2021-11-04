6478687a0f6500becae995e2ffdfd29a5bfdab6f
Balaji Betadur <balajibetadur@gmail.com>
2021-11-04 14:13:12 +0530

    2nd version responsive

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+let barLength = 300;
+
 let data = {
     'menu' : [
-        {class : 'uil uil-estate icon', 'text' : 'Home'},
-        {class : 'uil uil-user icon', 'text' : 'About'},
-        {class : 'uil uil-file-alt icon', 'text' : 'Skills'},
-        {class : 'uil uil-briefcase-alt icon', 'text' : 'Work'},
-        {class : 'uil uil-browser icon', 'text' : 'Projects'},
-        {class : 'uil uil-trophy icon', 'text' : 'Awards'},
-        {class : 'uil uil-message icon', 'text' : 'Contact'}
+        {'class' : 'uil uil-estate icon', 'text' : 'Home'},
+        {'class' : 'uil uil-user icon', 'text' : 'About'},
+        {'class' : 'uil uil-file-alt icon', 'text' : 'Skills'},
+        {'class' : 'uil uil-briefcase-alt icon', 'text' : 'Work'},
+        {'class' : 'uil uil-browser icon', 'text' : 'Projects'},
+        {'class' : 'uil uil-trophy icon', 'text' : 'Awards'},
+        {'class' : 'uil uil-message icon', 'text' : 'Contact'}
 
     ],
     'socialMedia' : [
@@ -60,24 +62,24 @@ let data = {
         }
     ],
     'skills' : [
-        {'skill' : 'Pyhton', 'Level' : 400 * 0.9},
-        {'skill' : 'Machine Learning', 'Level' : 400 * 0.8},
-        {'skill' : 'Deep Learning', 'Level' : 400 * 0.8},
-        {'skill' : 'Keras', 'Level' : 400 * 0.7},
-        {'skill' : 'Tableau', 'Level' : 400 * 0.7},
-        {'skill' : 'Neural Networks', 'Level' : 400 * 0.75},
-        {'skill' : 'SQL', 'Level' : 400 * 0.8},
-        {'skill' : 'Flask', 'Level' : 400 * 0.8},
-        {'skill' : 'Javascript', 'Level' : 400 * 0.9},
-        {'skill' : 'ReactJS', 'Level' : 400 * 0.8},
-        {'skill' : 'MongoDB', 'Level' : 400 * 0.8},
-        {'skill' : 'DBMS', 'Level' : 400 * 0.9},
-        {'skill' : 'Firebasae', 'Level' : 400 * 0.8},
-        {'skill' : 'AWS', 'Level' : 400 * 0.5},
-        {'skill' : 'Bootstrap', 'Level' : 400 * 0.9},
-        {'skill' : 'Git', 'Level' : 400 * 0.8},
-        {'skill' : 'Mentoring', 'Level' : 400 * 1},
-        {'skill' : 'Teaching', 'Level' : 400 * 1}
+        {'skill' : 'Pyhton', 'Level' :  0.9},
+        {'skill' : 'Machine Learning', 'Level' :  0.8},
+        {'skill' : 'Deep Learning', 'Level' :  0.8},
+        {'skill' : 'Keras', 'Level' :  0.7},
+        {'skill' : 'Tableau', 'Level' :  0.7},
+        {'skill' : 'Neural Networks', 'Level' :  0.75},
+        {'skill' : 'SQL', 'Level' :  0.8},
+        {'skill' : 'Flask', 'Level' :  0.8},
+        {'skill' : 'Javascript', 'Level' :  0.9},
+        {'skill' : 'ReactJS', 'Level' :  0.8},
+        {'skill' : 'MongoDB', 'Level' :  0.8},
+        {'skill' : 'DBMS', 'Level' :  0.9},
+        {'skill' : 'Firebasae', 'Level' :  0.8},
+        {'skill' : 'AWS', 'Level' :  0.5},
+        {'skill' : 'Bootstrap', 'Level' :  0.9},
+        {'skill' : 'Git', 'Level' :  0.8},
+        {'skill' : 'Mentoring', 'Level' :  1},
+        {'skill' : 'Teaching', 'Level' :  1}
     ]
     ,
     'work' : 
@@ -205,75 +207,117 @@ let data = {
                 'purpose' : 'just',
                 'description' : 'none',
                 'outcome' : 'none too',
-                'image' : 'autoML.jpg'
+                'image' : 'autoML.jpg',
+                'skills' : ''
             },
             {
                 'title' : 'COVID-19 prediction',
                 'purpose' : 'just',
                 'description' : 'none',
                 'outcome' : 'none too',
-                'image' : 'covid.jpg'
+                'image' : 'covid.jpg',
+                'skills' : ''
             },
             {
                 'title' : 'Lookinat - Abrod study prep website',
                 'purpose' : 'just',
                 'description' : 'none',
                 'outcome' : 'none too',
-                'image' : 'ms.jpg'
+                'image' : 'ms.jpg',
+                'skills' : ''
             },
             {
                 'title' : 'Moving Window Object detection',
                 'purpose' : 'just',
                 'description' : 'none',
                 'outcome' : 'none too',
-                'image' : 'object.png'
+                'image' : 'object.png',
+                'skills' : ''
             },
             {
                 'title' : 'Automation',
                 'purpose' : 'just',
                 'description' : 'none',
                 'outcome' : 'none too',
-                'image' : 'automate.jpg'
+                'image' : 'automate.jpg',
+                'skills' : ''
             },
             {
                 'title' : 'Spotty - Desktop Assisstant',
                 'purpose' : 'just',
                 'description' : 'none',
                 'outcome' : 'none too',
-                'image' : 'spotty.jpg'
+                'image' : 'spotty.jpg',
+                'skills' : ''
             },
             {
                 'title' : 'Human Machine Interaction System',
                 'purpose' : 'just',
                 'description' : 'none',
                 'outcome' : 'none too',
-                'image' : 'hm.jpg'
+                'image' : 'hm.jpg',
+                'skills' : ''
             },
             {
                 'title' : 'Buggy - Debugging programming errors',
                 'purpose' : 'just',
                 'description' : 'none',
                 'outcome' : 'none too',
-                'image' : 'debug.jpg'
+                'image' : 'debug.jpg',
+                'skills' : ''
             },
             {
                 'title' : 'Auto EDA',
                 'purpose' : 'just',
                 'description' : 'none',
                 'outcome' : 'none too',
-                'image' : 'eda.jpg'
+                'image' : 'eda.jpg',
+                'skills' : ''
             }
+        ],
+
+        'Achievements' : [
+            'achievement 1',
+            'achievement 1',
+            'achievement 1',
+            'achievement 1',
+            'achievement 1',
+            'achievement 1'
+        ],
+        'Certifications' : [
+            'Certification 1',
+            'Certification 1',
+            'Certification 1',
+            'Certification 1',
+            'Certification 1',
         ]
 
 
 }
+
+let certi = document.getElementById('certi__cards')
+data.Certifications.forEach(certi_ => {
+    console.log(certi)
+    certi.innerHTML += '<div class="certi__card">'+certi_+'</div>'
+})
+
+let ach = document.getElementById('achs')
+
+data.Achievements.forEach(achi => {
+    ach.innerHTML += '<p class = "achievement">'+achi+'</p></br>'
+})
+
+let header = document.getElementById('header');
+data.menu.forEach(option => {
+    header.innerHTML += '<span class = "main__option"><a  href = "">'+option.text+'</a></span>'
+})
 
 data.socialMedia.forEach(icon => {
     document.getElementById('social__media').innerHTML += "<a href=''' target='_blank'><i class='"+icon+"'></i></a>"
 });
 
 data.menu.forEach(icon => {
-    document.getElementById('menu').innerHTML += "<div class='icon__text'><i class='"+icon.class+"'></i>" + icon.text + "</div>" 
+    document.getElementById('menu').innerHTML += "<div class='icon__text'><a href=''> <i class='"+icon.class+"'></i></a>" + icon.text + "</div>" 
 });
 
 let menuIcon = document.getElementById('open__menu')
@@ -305,21 +349,42 @@ if (resumeBtn){
     })
 }
 
-whatIDo = document.getElementById('about')
+
+whatIDo = document.getElementById('about__data')
 
 data.whatIdo.forEach(item => {
-    whatIDo.innerHTML += '<div class="DSAI" id="DSAI"><p class="what__i__do__title" id="what__i__do__title">'+item.title+'</p><img class = "DSAI__img" id ="DSAI__img" src="images/'+item.image+'" alt="" srcset=""></div>';
+    temp = '<p class="what__i__do__title" id="what__i__do__title">'+item.title+'</p><div class = "d" id="d">'
 
+    temp += '<img class = "DSAI__img" id ="DSAI__img" src="images/'+item.image+'" alt="" srcset=""><div class="about__desc" id="about__desc">';
+
+    // ele = document.getElementById('about__desc')
     item.description.forEach(desc => {
-        whatIDo.innerHTML += '</br><p class ="description" >'+desc+'</p>'
+       temp += '</br><p class ="about__description" >'+desc+'</p>'
     })
+
+    temp += '</div>'
+    whatIDo.innerHTML += temp
+
 })
+
+// data.whatIdo.forEach(item => {
+//     whatIDo.innerHTML += '<p class="what__i__do__title" id="what__i__do__title">'+item.title+'</p><div class = "d" id="d"></div>'
+
+//     let ele = document.getElementById('d')
+//     ele.innerHTML += '<img class = "DSAI__img" id ="DSAI__img" src="images/'+item.image+'" alt="" srcset=""><div clas="about__desc" id="about__desc"></div>';
+
+//     ele = document.getElementById('about__desc')
+//     item.description.forEach(desc => {
+//         ele.innerHTML += '</br><p class ="description" >'+desc+'</p>'
+//     })
+// })
 
 skillBars = document.getElementById('skillBars');
 
 data.skills.forEach(skill => {
-    skillBars.innerHTML += '<div class = "bars"><div class="fullBar" id="fullBar"><div style = "width: '+skill.Level+'px" class="bar" id="bar"> <p class="skills__text" id="skills__text">'+skill.skill+'</p></div></div></div>'
+    skillBars.innerHTML += '<div class = "bars"><div class="fullBar" id="fullBar"><div style = "width: '+skill.Level * 300+'px" class="bar" id="bar"> <p class="skills__text" id="skills__text">'+skill.skill+'</p></div></div></div>'
 })
+
 
 let works = document.getElementById('r__internships')
 
@@ -332,7 +397,7 @@ works = document.getElementById('i__internships')
 
 data.work.Industrial_Internships.forEach(work => {
     
-        works.innerHTML += '<div class="card" id="card"><div class="card__name" id="card__name">'+work.location+'</div> <img class = "card__img"src="images/'+work.image+'" alt="" srcset=""> <div class="details" id="details"> <p class="location" id="location"> '+work.name+'</p><p class="duration" id="duration"> '+work.duration+' </p> </div></div><div class="modal" id="modal"><div class="modal__content" id="modal-content"> <i class="uil uil-times modal__close"></i><p class="modal__title" id="modal__title">'+work.name+'</p></br> <p class="outcome" id="outcome">Description</p><hr>'+work.description+' <p class="outcome" id="outcome">Outcome</p><hr>'+work.outcome+'</div> </div>'
+        works.innerHTML += '<div class="card" id="card"><div class="card__name" id="card__name">'+work.location+'</div> <img class = "card__img"src="images/'+work.image+'" alt="" srcset=""> <div class="details" id="details"> <p class="location" id="location"> '+work.name+'</p><p class="duration" id="duration"> '+work.duration+' </p> </div></div><div class="modal" id="modal"><div class="modal__content" id="modal-content"> <i class="uil uil-times modal__close"></i><p class="modal__title" id="modal__title">'+work.name+'</p></br> <div class="modal__set" id="modal__set"> <div><p class="outcome" id="outcome">Description</p><hr>'+work.description+' </div><div><p class="outcome" id="outcome">Outcome</p><hr>'+work.outcome+'</div> </div></div></div>'
 
         
     })
@@ -363,7 +428,7 @@ close.forEach((card,i) => card.addEventListener('click', () => {
     let projects = document.getElementById('project__cards')
     
     data.projects.forEach(project => {
-        projects.innerHTML += '<div style="background-image: url(images/'+project.image+');" class="project__card" id="project__card"><p class="project__name" id="project__name">'+project.title+'</p></div><div class="project__modal" id="modal"><div class="modal__content" id="modal-content"> <i class="uil uil-times project__modal__close"></i><p class="modal__title" id="modal__title">'+project.title+'</p></br> <p class="outcome" id="outcome">Description</p><hr>'+project.description+' <p class="outcome" id="outcome">Purpose</p><hr>'+work.purpose+'<hr><p class="outcome" id="outcome">Outcome</p><hr>'+project.outcome+'</div> </div>'
+        projects.innerHTML += '<div style="background-image: url(images/'+project.image+');" class="project__card" id="project__card"><p class="project__name" id="project__name">'+project.title+'</p></div><div class="project__modal" id="modal"><div class="modal__content" id="modal-content"> <i class="uil uil-times project__modal__close"></i><p class="modal__title" id="modal__title">'+project.title+'</p></br> <div class="modal__set" id=modal__set"><div><p class="outcome" id="outcome">Description</p><hr>'+project.description+' </div><div><p class="outcome" id="outcome">Purpose</p><hr>'+work.purpose+'</div><div><p class="outcome" id="outcome">Outcome</p><hr>'+project.outcome+'</div></div> <div class="github" id="github"><i class="uil uil-github icon__github"><span class="github__text">   GitHub</span></i></div></div></div>'
     })
     
 let projectCards = document.querySelectorAll('.project__card');
